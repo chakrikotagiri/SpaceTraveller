012fc70cda5057c2dacaaba6f69cb1331d25fb1a
Chakri Kotagiri <chakri.kotagiri@gmail.com>
2017-07-08 09:38:44 +0530

    New Project

---
+++ SpaceDAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpaceDAL
+{
+    public class Class1
+    {
+        public string GreetUser()
+        {
+            return "Hello Martian";
+        }
+    }
+}
---
+++ SpaceTraveller/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SpaceDAL;
+namespace SpaceTraveller
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WelcomeUser();
+        }
+
+        private void WelcomeUser()
+        {
+            SpaceDAL.Class1 c = new Class1();
+            Label1.Text = c.GreetUser();
+        }
+    }
+}
